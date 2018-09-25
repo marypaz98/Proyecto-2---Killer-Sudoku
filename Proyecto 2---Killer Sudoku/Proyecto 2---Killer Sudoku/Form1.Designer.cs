7acfa41ad5a4d1bde846dc565f26595d795c7352
marypaz98 <marypaz-aghe@hotmail.com>
2018-09-25 00:42:43 -0600

    Creación del proyecto

---
+++ Proyecto 2---Killer Sudoku/Proyecto 2---Killer Sudoku/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Proyecto_2___Killer_Sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonResolver = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxHilos = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxTamaño = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(1, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(366, 431);
+            this.panel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(410, 2);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(383, 431);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // buttonResolver
+            // 
+            this.buttonResolver.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonResolver.Font = new System.Drawing.Font("Maiandra GD", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonResolver.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonResolver.Location = new System.Drawing.Point(822, 178);
+            this.buttonResolver.Name = "buttonResolver";
+            this.buttonResolver.Size = new System.Drawing.Size(98, 44);
+            this.buttonResolver.TabIndex = 2;
+            this.buttonResolver.Text = "Resolver";
+            this.buttonResolver.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Maiandra GD", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(814, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hilos:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxHilos
+            // 
+            this.textBoxHilos.Font = new System.Drawing.Font("Maiandra GD", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxHilos.Location = new System.Drawing.Point(871, 12);
+            this.textBoxHilos.Name = "textBoxHilos";
+            this.textBoxHilos.Size = new System.Drawing.Size(155, 21);
+            this.textBoxHilos.TabIndex = 4;
+            this.textBoxHilos.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Maiandra GD", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(818, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tamaño:";
+            // 
+            // textBoxTamaño
+            // 
+            this.textBoxTamaño.Font = new System.Drawing.Font("Maiandra GD", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTamaño.Location = new System.Drawing.Point(898, 92);
+            this.textBoxTamaño.Name = "textBoxTamaño";
+            this.textBoxTamaño.Size = new System.Drawing.Size(128, 21);
+            this.textBoxTamaño.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Font = new System.Drawing.Font("Maiandra GD", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(822, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 41);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1038, 459);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxTamaño);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxHilos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonResolver);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Maiandra GD", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button buttonResolver;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxHilos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxTamaño;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
